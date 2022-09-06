e362194e1dd744c3031399c7a62c073c8db04667
chengjin ren <2541123296@qq.com>
2022-09-06 17:47:35 +0800

    policy_evaluate policy iteration value iteration

---
+++ reinforce/codes_for_book/c03/Dynamic_Programming.py
@@ -0,0 +1,80 @@
+#encoding:utf-8
+
+#动态规划进行策略评估、策略迭代和价值迭代
+
+#1 小型方格世界MDP建模
+
+# 4*4 方格状态命名
+# 状态0和15为终止状态
+#  0  1  2  3
+#  4  5  6  7
+#  8  9 10  11
+# 12 13 14  15
+
+from utils import policy_evaluate
+from utils import display_V
+from utils import P
+from utils import R
+from utils import uniform_random_pi
+from utils import greedy_pi
+from utils import policy_iterate
+from utils import value_iterate
+from utils import greedy_policy
+from utils import display_policy
+
+S = [i for i in range(16)]
+A = ["n", "e", "s", "w"] 
+
+gamma = 1.00
+MDP = S, A, P, R, gamma
+
+#2.策略评估
+
+#异步动态规划，使用最近更新的状态价值，更新当前状态价值
+V = [0  for _ in range(16)] # 状态价值初始化
+V_pi = policy_evaluate(MDP, V, uniform_random_pi, 100)
+display_V(V_pi)
+
+V = [0  for _ in range(16)] # 状态价值
+V_pi = policy_evaluate(MDP, V, greedy_pi, 100)
+display_V(V_pi)
+
+#3.策略迭代
+V = [0  for _ in range(16)] # 重置状态价值
+V_pi = policy_iterate(MDP, V, greedy_pi, 1, 100)
+print("optimal value: ")
+print("")
+display_V(V_pi)
+
+#4 价值迭代
+print("价值迭代")
+V = [0  for _ in range(16)] # 重置状态价值
+display_V(V)
+
+V_star = value_iterate(MDP, V, 4)
+print("value iteration process: ")
+print("")
+display_V(V_star)
+
+
+print("显示状态对应的行为：")
+print("")
+display_policy(greedy_policy, MDP, V_star)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- reinforce/codes_for_book/c03/utils.py
+++ reinforce/codes_for_book/c03/utils.py
@@ -1,3 +1,28 @@
+ds_actions = {"n": -4, "e": 1, "s": 4, "w": -1} # 行为对状态的改变
+
+def dynamics(s, a): # 环境动力学
+    '''模拟小型方格世界的环境动力学特征
+    Args:
+        s 当前状态 int 0 - 15
+        a 行为 str in ['n','e','s','w'] 分别表示北、东、南、西
+    Returns: tuple (s_prime, reward, is_end)
+        s_prime 后续状态
+        reward 奖励值
+        is_end 是否进入终止状态
+    '''
+    s_prime = s
+    if (s%4 == 0 and a == "w") or (s<4 and a == "n") \
+       or ((s+1)%4 == 0 and a == "e") or (s > 11 and a == "s") \
+       or s in [0, 15]:
+        pass
+    else:
+        ds = ds_actions[a]
+        s_prime = s + ds
+    
+    reward  = 0 if s in [0,15] else -1
+    is_end = True if s in [0,15] else False
+    return s_prime,reward,is_end
+
 def P(s, a, s1):
     s_prime, _, _ = dynamics(s, a)
     return s1 == s_prime
@@ -12,7 +37,19 @@ def get_prob(P, s, a, s1):
 def get_reward(R, s, a):
     return R(s, a)
 
+def display_V(V): # 显示状态价值
+    for i in range(16):
+        print('{0:>6.2f}'.format(V[i]),end = " ")
+        if (i+1) % 4 == 0:
+            print("")
+    print() 
+           
 ########################
+def set_value(V, s, v): # 设置价值字典
+    V[s] = v
+    
+def get_value(V, s): # 获取状态价值
+    return V[s]
 
 def get_pi(Pi, s, a, MDP = None, V = None):
     return Pi(MDP, V, s, a)
@@ -25,19 +62,22 @@ def uniform_random_pi(MDP = None, V = None, s = None, a = None):
 def greedy_pi(MDP, V, s, a):
     S, A, P, R, gamma = MDP
     max_v, a_max_v = -float('inf'), []
-    for a_opt in A:# 统计后续状态的最大价值以及到达到达该状态的行为（可能不止一个）
-        s_prime, reward, _ = dynamics(s, a_opt)
-        if V[s_prime] > max_v:
-            max_v = V[s_prime]
+    for a_opt in A:# 统计后续状态的最大价值以及到达该状态的行为（可能不止一个）
+        s_prime, _, _ = dynamics(s, a_opt)
+        v_s_prime = get_value(V, s_prime)
+        if v_s_prime > max_v:
+            max_v = v_s_prime
             a_max_v = [a_opt]
-        elif(V[s_prime] == max_v):
+        elif(v_s_prime == max_v):
             a_max_v.append(a_opt)
     n = len(a_max_v)
     if n == 0: return 0.0
     return 1.0/n if a in a_max_v else 0.0
 
-
 def epsilon_greedy_pi(MDP, V, s, a, epsilon = 0.1):
+    '''
+    this method compare to greedy_pi
+    '''
     if MDP is None:
         return 0.0
     _, A, _, _, _ = MDP
@@ -48,22 +88,20 @@ def epsilon_greedy_pi(MDP, V, s, a, epsilon = 0.1):
     # n = int(1.0/greedy_p)
     return (1 - epsilon + epsilon/m) * greedy_p
 
-
 def compute_q(MDP, V, s, a):
     '''根据给定的MDP，价值函数V，计算状态行为对s,a的价值qsa
     '''
-    S, A, R, P, gamma = MDP
+    S, A, P, R, gamma = MDP
     q_sa = 0
     for s_prime in S:
         q_sa += get_prob(P, s, a, s_prime) * get_value(V, s_prime)
     q_sa = get_reward(R, s,a) + gamma * q_sa
     return q_sa
 
-
 def compute_v(MDP, V, Pi, s):
     '''给定MDP下依据某一策略Pi和当前状态价值函数V计算某状态s的价值
     '''
-    S, A, R, P, gamma = MDP
+    S, A, P, R , gamma = MDP
     v_s = 0
     for a in A:
         v_s += get_pi(Pi, s, a, MDP, V) * compute_q(MDP, V, s, a)
@@ -78,7 +116,6 @@ def update_V(MDP, V, Pi):
         set_value(V_prime, s, compute_v(MDP, V_prime, Pi, s))
     return V_prime
 
-
 def policy_evaluate(MDP, V, Pi, n):
     '''使用n次迭代计算来评估一个MDP在给定策略Pi下的状态价值，初始时价值为V
     '''
@@ -89,27 +126,28 @@ def policy_evaluate(MDP, V, Pi, n):
     return V
 
 def policy_iterate(MDP, V, Pi, n, m):
-    cur_Pi = Pi
     for i in range(m):
         V = policy_evaluate(MDP, V, Pi, n)
-        Pi = epsilon_greedy_pi
+        Pi =  greedy_pi #greedy_pi epsilon_greedy_pi
         #print("改善了策略")
     return V
 
 # 价值迭代得到最优状态价值过程
 def compute_v_from_max_q(MDP, V, s):
-    '''根据一个状态的下所有可能的行为价值中最大一个来确定当前状态价值
     '''
-    S, A, R, P, gamma = MDP
+    根据一个状态的下所有可能的行为价值中最大一个来确定当前状态价值
+    '''
+    S, A, P ,R, gamma = MDP
     v_s = -float('inf')
     for a in A:
         qsa = compute_q(MDP, V, s, a)
-        if qsa >= v_s:
+        if qsa > v_s:
             v_s = qsa
     return v_s
 
 def update_V_without_pi(MDP, V):
-    '''在不依赖策略的情况下直接通过后续状态的价值来更新状态价值
+    '''
+    在不依赖策略的情况下直接通过后续状态的价值来更新状态价值
     '''
     S, _, _, _, _ = MDP
     V_prime = V.copy()
@@ -124,3 +162,27 @@ def value_iterate(MDP, V, n):
         V = update_V_without_pi(MDP, V)
         display_V(V)
     return V
+
+def greedy_policy(MDP,V,s):
+    S,A,P,R,gamma = MDP
+    max_v, a_max_v = -float('inf'), []
+    for a_opt in A:# 统计后续状态的最大价值以及到达到达该状态的行为（可能不止一个）
+        s_prime, _, _ = dynamics(s, a_opt)
+        v_s_prime = get_value(V, s_prime)
+        if v_s_prime > max_v:
+            max_v = v_s_prime
+            a_max_v = a_opt
+        elif(v_s_prime == max_v):
+            a_max_v += a_opt
+    return str(a_max_v)
+
+def display_policy(policy, MDP, V):
+    S, A, P, R, gamma = MDP
+    for i in range(16):
+        print('{0:^6}'.format(policy(MDP, V, S[i])),end = " ")
+        if(i + 1) % 4 == 0:
+            print("")
+    print()
+    
+    
+
